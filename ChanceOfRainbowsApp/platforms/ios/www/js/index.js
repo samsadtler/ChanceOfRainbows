16accba759debb4af862de639164973d888443e1
Sam Sadtler <sam.sadtler@gmail.com>
2016-05-02 10:46:01 -0400

    getting everything in the right place

--- ChanceOfRainbowsApp/platforms/ios/www/js/index.js
+++ ChanceOfRainbowsApp/platforms/ios/www/js/index.js
@@ -36,8 +36,7 @@ var app = {
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
         document.addEventListener('deviceready', this.getLocation, false);
-        document.addEventListener('deviceready', this.touchHandler, false);
-        document.addEventListener('deviceready', this.dynamicElements, false);
+        // document.addEventListener('deviceready', this.touchHandler, false);
     },
     // deviceready Eventvent Handler
     //
@@ -50,14 +49,10 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        // var parentElement = document.getElementById(id);
-        // var listeningElement = parentElement.querySelector('.listening');
-        // var receivedElement = parentElement.querySelector('.received');
+        console.log('retrieve event')
+    },
+    eventTiming: function(e){
 
-        // listeningElement.setAttribute('style', 'display:none;');
-        // receivedElement.setAttribute('style', 'display:block;');
-
-        // console.log('Received Event: ' + id);
     },
     touchHandler: function(e){
         console.log("start touchHandler")
@@ -78,14 +73,14 @@ var app = {
             var dist = parseInt(touchobj.clientY) - starty
             // console.log("move dist " + dist) 
             touchState = true;
-            if (dist > 50 ) {
-                app.fadeEvent(starty, dist);
-                console.log('down touch detected') 
-            }
-            else if (dist < -50 ) {   
-                app.fadeEvent(starty, dist);
-                console.log('up touch detected')             
-            }
+            // if (dist > 50 ) {
+            //     app.fadeEvent(starty, dist);
+            //     console.log('down touch detected') 
+            // }
+            // else if (dist < -50 ) {   
+            //     app.fadeEvent(starty, dist);
+            //     console.log('up touch detected')             
+            // }
             e.preventDefault()
         }, false)
 
@@ -93,42 +88,33 @@ var app = {
             var touchobj = e.changedTouches[0] // reference first touch point for this event
             var dist = parseInt(touchobj.clientY) - starty
              console.log("move dist " + dist)
-            // if (dist > 110 ) {
-            //     app.fadeEvent(starty, dist);
-            //     console.log('down touch detected' , e) 
-            // }
-            // else if (dist < -110 ) {   
-            //     app.fadeEvent(starty, dist);
-            //     console.log('up touch detected' , e)             
-            // }
+            if (dist > 110 ) {
+                app.fadeEvent(starty, dist);
+                console.log('down touch detected' , e) 
+            }
+            else if (dist < -110 ) {   
+                app.fadeEvent(starty, dist);
+                console.log('up touch detected' , e)             
+            }
            e.preventDefault()
         }, false)
-         jQuery('ul').on('tap', 'img', function(){
-            console.log("tap it baby")                 
-        });
-    },
-    dynamicElements: function(){
-        console.log('dynamicElements')
-        var chevronToAppend = 
-            '<div>'+
-                    // '<hr class="catchadream" style="height:20px;border:none;width:100%">'+'</hr>'+
-                  '<i class=" arrow fa fa-chevron-down text-center col-xs-12" aria-hidden="true">'+'</i>'+
-             '</div>';
-        jQuery('.arrow').append(chevronToAppend); 
+        //  jQuery('ul').on('tap', 'img', function(){
+        //     console.log("tap it baby")                 
+        // });
     },
     fadeEvent: function( touchStart, touchDist){
         console.log('dist received')
         var arrowToFade = document.querySelector(".arrow");
         // var elementToFade = document.querySelector(".question");
-        // var cardToFade = document.querySelector(".card-holder");
+        var cardToFade = document.querySelector(".card-holder");
         var windowHeight = window.innerHeight;
         console.log ('window Height',  windowHeight)
         var start = touchStart;
         var delta = touchDist;
-        if(delta < 0){
+        if(delta < 600){
           opacityVal = windowHeight/screenHeight;  
         }
-        if(delta > 0){
+        if(delta > 600){
           opacityVal = delta/screenHeight;  
         }
         console.log("scroll value = " + top);
@@ -136,7 +122,7 @@ var app = {
         if (opacityVal > .1 && opacityVal < 1){
             arrowToFade.style.opacity = opacityVal;
             // elementToFade.style.opacity = opacityVal;
-            // cardToFade.style.opacity = 1/opacityVal;
+            cardToFade.style.opacity = 1/opacityVal;
         } 
         if (opacityVal >= 1){
             arrowToFade.style.opacity = 0.0;
@@ -146,11 +132,13 @@ var app = {
         else {
             arrowToFade.style.opacity = 1.0;
             // elementToFade.style.opacity = 1.0;
-            // cardToFade.style.opacity = 0.0;
+            cardToFade.style.opacity = 0.0;
         }
     },
     geometryPowerHorizon: function(alt){
-        console.log('geometryPower now')
+        // console.log('geometryPower now')
+        //calculates the visible horizon based on current altitude
+        // in order to compare it with the sun angle
         var R = 6371000;// earths radius (m)
         var ph = 1.7;// average person height (m) 
         var a =  alt; // altitude of observer (m)
@@ -166,7 +154,7 @@ var app = {
     },
     suncalcRequest: function(lat, lng, yearmonthday, alt){
         var localhost = "https://chanceofrainbows.herokuapp.com";
-
+        //need sunset
         console.log('request --> lat: ', lat,'lng: ', lng, 'date: ', yearmonthday);
         dataArray = {
             'lat': lat,
@@ -277,10 +265,10 @@ var app = {
             }
             
             if(!sunCondition) {
-                answerArray = outcome.response;
+                answerArray = window.out.answer.night.response;
                 shortAnswer = answerArray[0];
                 longAnswer = answerArray[app.getRandomInt(1, answerArray.length)]
-                rainbowChance = "Its the wrong time of day!";
+                rainbowChance = "Try again later in the day!";
                 return app.addCard(rainbowChance, shortAnswer, longAnswer)
             }
             console.log('sunCondition: ', sunCondition, "cloudCover: ", cloudCover, "precipitationChance: ", precipitationChance )
@@ -295,7 +283,7 @@ var app = {
         var rainbowAngle = 42.3; // angle of rainbow from shadow hypotenuse (deg)
         if(sunAngle < (observerAngle + rainbowAngle) && sunAngle > 0){
             console.log('Theresome chance of rainbow!')
-           return sunCondition = true;
+            return sunCondition = true;
 
         }
         if(sunAngle > (observerAngle + rainbowAngle) || sunAngle < 0){
@@ -322,33 +310,6 @@ var app = {
             return alert("Geolocation is not supported by this browser.");
         }
     },
-    // geoCodeIt: function(location){
-    //     console.log("geoCodeIt");
-    //     var apiKey = 'AIzaSyCIxywgknotMlV6Kjqn-HbJgQBkSAMPOlU';
-
-    //     // make a request to geocode the location
-    //     jQuery.ajax({
-    //         url: 'https://maps.googleapis.com/maps/api/geocode/json?address='+location+'&key='+apiKey,
-    //         type: 'GET',
-    //         failure: function(err){
-    //             return alert ("Could not find that location");
-    //         },
-    //         success: function(response) {
-
-    //           console.log('the geocode response is -- >');
-    //           console.log(response);
-              
-    //           if(response.status=="ZERO_RESULTS") return alert ("Could not find that location");
-
-    //           // now that we have the lat/lon details, can get the weather
-    //           var lat = response.results[0].geometry.location.lat;
-    //           var lon = response.results[0].geometry.location.lng;
-    //           console.log('lat and lon from geomentry google balls: ', lat, ' ',lon )
-    //           // return app.getTheWeatherAPI(location, lat, lon);
-    //         }
-    //     });
-    // },
-
     getTheWeatherAPI: function(location, lat, lon){
         console.log('getTheWeatherAPI')
         //forecast apiKey
@@ -426,16 +387,19 @@ var app = {
 
     addCard: function(rainbowChance, shortAnswer, longAnswer){
         jQuery('.short-answer').text(shortAnswer);
-        var chanceToAppend = 
-             
-                  '<p class=" text-centered rainbow-chance">'+ rainbowChance +'</p>';
-           
-        jQuery('.rainbow-chance').append(chanceToAppend);   
+        var chanceToAppend =  
+            '<p class=" text-centered rainbow-chance">'+ rainbowChance +'</p>';      
+        
+        jQuery('.rainbow-chance').append(chanceToAppend);
+        var chevronToAppend =  
+            '<i class="arrow chevron fa fa-chevron-down text-centered" aria-hidden="true"></i>';      
+        jQuery('.chevron').append(chevronToAppend);
+       
         var htmlToAppend = 
         '<div class="card-container col-sx-12 centered">'+
             '<div class="card">'+
                     // '<img src="img/'+icon+'" class="img-responsive">'+
-                '<h1>'+longAnswer+'</h1>'+
+                '<h1 class="long-answer">'+longAnswer+'</h1>'+
           '</div>'+
         '</div>';
         jQuery('.card-holder').append(htmlToAppend);
